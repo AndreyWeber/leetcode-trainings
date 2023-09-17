3a0d5c134c7ac2b4924964501a60c2cce2a53bc2
AndreyWeber <andrey.weber@gmail.com>
2023-09-17 21:52:10 +0100

    Added Reverse String task

---
+++ ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿var s = new[] { 'H', 'a', 'n', 'n', 'a', 'h' };
+
+Console.WriteLine(new string(s));
+
+ReverseString(s);
+
+Console.WriteLine(new string(s));
+Console.ReadKey(false);
+
+static void ReverseString(char[] s)
+{
+    if (s == null || !s.Any())
+    {
+        return;
+    }
+
+    var left = 0;
+    var right = s.Length - 1;
+    while (left <= right)
+    {
+        (s[right], s[left]) = (s[left], s[right]);
+        left++;
+        right--;
+    }
+}
