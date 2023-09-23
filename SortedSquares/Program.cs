6683fe6d43501cfa66e3eacc85ee2045715722d4
AndreyWeber <andrey.weber@gmail.com>
2023-09-23 16:23:05 +0100

    Added SortedSquares task

---
+++ SortedSquares/Program.cs
@@ -0,0 +1,29 @@
+﻿// var nums = new int[] { -7, -3, 2, 3, 11 };
+// var nums = new int[] { -5, -3, -2, -1 };
+var nums = new int[] { -10000, -9999, -7, -5, 0, 0, 10000 };
+var result = SortedSquares(nums);
+
+static int[] SortedSquares(int[] nums)
+{
+    var left = 0;
+    var right = nums.Length - 1;
+    var res = new int[nums.Length];
+
+    var val = 0;
+    for (var i = right; i >= 0; i--)
+    {
+        if (Math.Abs(nums[left]) < Math.Abs(nums[right]))
+        {
+            val = nums[right];
+            right--;
+        }
+        else
+        {
+            val = nums[left];
+            left++;
+        }
+        res[i] = val * val;
+    }
+
+    return res;
+}
